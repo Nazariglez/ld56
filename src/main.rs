8674765bc1dd13bde9bc1c22fd42e910a2d3c148
Nazari Gonzalez <nazari.nz@gmail.com>
2024-10-05 00:32:26 +0100

    engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rkit::draw::create_draw_2d;
+use rkit::gfx;
+use rkit::gfx::Color;
+use rkit::math::vec2;
+
+fn main() -> Result<(), String> {
+    rkit::init().on_update(update).run()
+}
+
+fn update(state: &mut ()) {
+    let mut draw = create_draw_2d();
+    draw.clear(Color::BLACK);
+    gfx::render_to_frame(&draw).unwrap();
 }
